6fe557b0896c0ad07ff5c26bd95b2e3a825da855
Jeffrey Obeng <jeffrey33obeng@gmail.com>
2026-02-18 12:19:59 +0000

    Remove this if it don't play any role there and make fix the errors: Ru

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -62,8 +62,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Chevron: ({ ...props }) => {
-          if (props.orientation === "left") {
+        Chevron: ({ orientation }) => {
+          if (orientation === "left") {
             return <ChevronLeft className="h-4 w-4" />
           }
           return <ChevronRight className="h-4 w-4" />
